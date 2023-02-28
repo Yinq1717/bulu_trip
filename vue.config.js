8a3c8574408653ab37ce89b993d13a745e60d0da
“bulu191” <2083453529@qq.com>
2023-02-28 19:51:57 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
+function resolve(dir) {
+   return path.join(__dirname, dir);
+}
+
+module.exports = defineConfig({
+   transpileDependencies: true,
+   configureWebpack: {
+      // provide the app's title in webpack's name field, so that
+      // it can be accessed in index.html to inject the correct title.
+      resolve: {
+         alias: {
+            "@": resolve("src"),
+         },
+      },
+   },
+});
